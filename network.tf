c677cbdd6b722a013b2b03affd9b81124ff6879b
xXxSpikyxXx <113418385+xXxSpikyxXx@users.noreply.github.com>
2022-09-14 01:16:38 +0800

    Delete network.tf

--- network.tf
+++
@@ -1,7 +0,0 @@
-module "network" {
-  source  = "app.terraform.io/PCSSecurity/network/azurerm"
-  version = "3.5.0"
-  # insert required variables here
-  resource_group_name = "${var.prefix}-workshop"
-
-}
